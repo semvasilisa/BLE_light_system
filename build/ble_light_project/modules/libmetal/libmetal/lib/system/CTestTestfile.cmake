24b64d09b7afd2c245eff8addddf68a41825bed1
Liss <semvasiliska@gmail.com>
2026-08-23 13:38:57 +0200

    Stop tracking build directory

--- build/ble_light_project/modules/libmetal/libmetal/lib/system/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ncs/v3.4.0/modules/hal/libmetal/libmetal/lib/system
-# Build directory: D:/projects/ble_light_project/build/ble_light_project/modules/libmetal/libmetal/lib/system
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("zephyr")
--- build/ble_light_project/modules/open-amp/open-amp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ncs/v3.4.0/modules/lib/open-amp/open-amp
-# Build directory: D:/projects/ble_light_project/build/ble_light_project/modules/open-amp/open-amp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
