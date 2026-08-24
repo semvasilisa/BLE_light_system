c2036de11e0801ffa43a315df8286f50f5944e4e
Liss <semvasiliska@gmail.com>
2026-08-24 13:42:42 +0200

    Remove build directory from repository

--- build/ble_light_project/modules/libmetal/libmetal/lib/compiler/armcc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ncs/v3.4.0/modules/hal/libmetal/libmetal/lib/compiler/armcc
-# Build directory: D:/projects/ble_light_project/build/ble_light_project/modules/libmetal/libmetal/lib/compiler/armcc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ble_light_project/modules/open-amp/open-amp/lib/virtio/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/ncs/v3.4.0/modules/lib/open-amp/open-amp/lib/virtio
-# Build directory: D:/projects/ble_light_project/build/ble_light_project/modules/open-amp/open-amp/lib/virtio
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
